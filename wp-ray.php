3d6823bb5b99ffb3ecc70cb862b178e22dc31619
freek <freek@spatie.be>
2021-01-09 16:40:57 +0100

    wip

---
+++ wp-ray.php
@@ -0,0 +1,9 @@
+/**
+* Plugin Name: wp-ray
+* Plugin URI: https://github.com/spatie/wordpress-ray
+* Description: Easily debug WordPress app
+* Version: 0.0.1
+* Author: Spatie
+* Author URI: http://spatie.be
+* License: MIT
+*/
