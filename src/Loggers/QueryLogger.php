caf1da0d8210a8c57743bb41390d800f3152b596
Mark Jaquith <mark@coveredwebservices.com>
2021-01-11 17:23:45 -0500

    Fix microseconds math bug

--- src/Loggers/QueryLogger.php
+++ src/Loggers/QueryLogger.php
@@ -55,7 +55,7 @@ class QueryLogger
 
     public function sendDetailedQueryToRay($data, $sql, $timeInSeconds): array
     {
-        $timeInMilliSeconds = $timeInSeconds / 1000;
+        $timeInMilliSeconds = $timeInSeconds * 1000;
 
         $payload = new ExecutedQueryPayload($sql, $timeInMilliSeconds);
 
